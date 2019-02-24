0f07fba03cb6795de75a2f674f49bc91254ca92b
Barry Ivan Silber <age.ef.ess@gmail.com>
2019-02-24 17:43:36 +0000

    Automatically backed up by Learn

--- lib/greeting.rb
+++ lib/greeting.rb
@@ -1,4 +1,4 @@
 # code the #greeting method here!
-puts "enter your name"
-name = gets.strip
-puts "Hello #{name}. It's nice to meet you."
+def greeting(name)
+  puts "Hello #{name.strip}. It's nice to meet you."
+end
